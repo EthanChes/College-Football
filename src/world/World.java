4823401974dd30e65336644811ba99ddb011a71d
EthanChes <echesney2023@cchsdons.com>
2021-11-26 17:27:50 -0800

    [Ethan] Finished Defensive Line (Run Rush)

--- src/world/World.java
+++ src/world/World.java
@@ -75,9 +75,8 @@ public class World {
                 }
             }
 
-            //Slants O_play = new Slants(194-2,-250);
-            //Four_Verticals O_play = new Four_Verticals(194 - 2,-250);
-            RB_Dive O_play = new RB_Dive(194 - 2,-250);
+            Four_Verticals O_play = new Four_Verticals(194 - 2,-250);
+            //RB_Dive O_play = new RB_Dive(194 - 2,-250);
             Line_Blitz D_play = new Line_Blitz(194 - 2,-250);
             entities.addAll(D_play.getEntities());
             entities.addAll(O_play.getEntities());
