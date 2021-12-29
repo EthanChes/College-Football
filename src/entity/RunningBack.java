dcb2440dc89d353cb027fa7c8d2f3082ffa35248
EthanChes <echesney2023@cchsdons.com>
2021-12-28 23:07:21 -0800

    [Ethan] Right Arrow To See Offensive Routes

--- src/entity/RunningBack.java
+++ src/entity/RunningBack.java
@@ -92,7 +92,7 @@ public class RunningBack extends Entity {
             movement.add(0,-speed*delta); // multiply by delta (framecap) to move 10 frames in a second.
         }
         if (window.getInput().isKeyDown(GLFW_KEY_A) && userControl) { // When A is pressed, camera shifts left 5
-            movement.add(-speed*delta,0);
+            movement.add(-speed*delta/2,0);
         }
         if (window.getInput().isKeyDown(GLFW_KEY_W) && userControl) { // When W is pressed, camera shifts up 5
             movement.add(0,speed*delta);
