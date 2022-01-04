5b7d1407d13be74900444363403e551b300e753e
EthanChes <echesney2023@cchsdons.com>
2022-01-04 00:49:55 -0800

    [Ethan] PlayClock Fix for Users

--- src/world/World.java
+++ src/world/World.java
@@ -619,13 +619,13 @@ public class World {
 
         // Set Timer on Playclock accordingly to play strategy
         int playTimeNew = 20;
-        if (GameManager.userHome && ! GameManager.userOffense) {
+        if ((GameManager.userHome && ! GameManager.userOffense) || (! GameManager.userHome && GameManager.userOffense)) {
             switch (GameManager.awayTimeStrategy) {
                 case 0 : playTimeNew = 15; break;
                 case 1 : playTimeNew = 20; break;
                 case 2 : playTimeNew = 10; break;
             }
-        } else if (! GameManager.userOffense && ! GameManager.userHome){
+        } else if ((! GameManager.userOffense && ! GameManager.userHome) || (GameManager.userHome && GameManager.userOffense)) {
             switch (GameManager.homeTimeStrategy) {
                 case 0 : playTimeNew = 15; break;
                 case 1 : playTimeNew = 20; break;
